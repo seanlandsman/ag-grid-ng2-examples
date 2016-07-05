8f030906fe668478a13f42e320816702bbe4bfff
Sean Landsman <sean@thelandsmans.com>
2016-07-05 10:45:59 +0100

    Initial setup

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,64 +1,50 @@
+/**
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ */
 (function (global) {
-
     // map tells the System loader where to look for things
     var map = {
-        'lib': 'lib',
-        'rxjs': 'node_modules/rxjs',
-        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+        'app': 'app', // 'dist',
         '@angular': 'node_modules/@angular',
-        'ag-grid-ng2': 'node_modules/ag-grid-ng2',
-        'ag-grid-enterprise': 'node_modules/ag-grid-enterprise',
-        'ag-grid': 'node_modules/ag-grid'
+        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+        'rxjs': 'node_modules/rxjs'
     };
-
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
+        'app': {main: 'main.js', defaultExtension: 'js'},
         'rxjs': {defaultExtension: 'js'},
-        'angular2-in-memory-web-api': {defaultExtension: 'js'},
-        lib: {
-            // format: 'register',
-            defaultExtension: 'js',
-            main: 'boot.js'
-        },
-        'ag-grid-ng2': {
-            defaultExtension: "js"
-        },
-        'ag-grid': {
-            defaultExtension: "js"
-        },
-        'ag-grid-enterprise': {
-            defaultExtension: "js"
-        }
+        'angular2-in-memory-web-api': {main: 'index.js', defaultExtension: 'js'},
     };
-
-    var packageNames = [
-        '@angular/common',
-        '@angular/compiler',
-        '@angular/core',
-        '@angular/http',
-        '@angular/platform-browser',
-        '@angular/platform-browser-dynamic',
-        '@angular/router',
-        '@angular/router-deprecated',
-        '@angular/testing',
-        '@angular/upgrade',
+    var ngPackageNames = [
+        'common',
+        'compiler',
+        'core',
+        'forms',
+        'http',
+        'platform-browser',
+        'platform-browser-dynamic',
+        'router',
+        'router-deprecated',
+        'upgrade',
     ];
+    // Individual files (~300 requests):
+    function packIndex(pkgName) {
+        packages['@angular/' + pkgName] = {main: 'index.js', defaultExtension: 'js'};
+    }
 
-    // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
-    packageNames.forEach(function (pkgName) {
-        packages[pkgName] = {main: 'index.js', defaultExtension: 'js'};
-    });
+    // Bundled (~40 requests):
+    function packUmd(pkgName) {
+        packages['@angular/' + pkgName] = {main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js'};
+    }
 
+    // Most environments should use UMD; some (Karma) need the individual index files
+    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+    // Add package entries for angular packages
+    ngPackageNames.forEach(setPackageConfig);
     var config = {
         map: map,
         packages: packages
-    }
-
-    // filterSystemConfig - index.html's chance to modify config before we register it.
-    if (global.filterSystemConfig) {
-        global.filterSystemConfig(config);
-    }
-
+    };
     System.config(config);
-
 })(this);
