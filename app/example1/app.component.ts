b113d4aede893cda6043c2ceba5c5a9a420de479
Sean Landsman <sean@thelandsmans.com>
2016-07-05 14:23:07 +0100

    Further work

--- app/example1/app.component.ts
+++ app/example1/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
+import {GridOptions} from 'ag-grid/main';
+import {AgGridNg2} from 'ag-grid-ng2/main';
+import {DataService} from "../common/DataService";
+
 
 @Component({
-  selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+    selector: 'my-app',
+    templateUrl: 'app/example1/example1.html',
+    directives: [<any>AgGridNg2],
+    providers: [DataService]
 })
-export class AppComponent { }
+export class AppComponent {
+    private gridOptions: GridOptions;
+    private columnDefs: any[];
+    private rowData:any[];
+
+    constructor(dataService:DataService) {
+        this.gridOptions = <GridOptions>{};
+        this.columnDefs = dataService.createSimpleColumnDefs();
+        this.rowData = dataService.createTestData(10);
+    }
+}
